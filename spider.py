b40caf337454e9d1d46f52064b637c0430e50ea0
xudong07 <xudong714@gmail.com>
2018-02-03 23:57:02 +0800

    recover from my code

--- spider.py
+++ spider.py
@@ -1,208 +1,189 @@
-import hashlib   
+'''煎蛋网图片爬虫'''
+import re
+import random
+import datetime
+import time
+import hashlib
 import base64
 from bs4 import BeautifulSoup
 import requests
-import re
-import random
-import shutil
-import os
-import time
-import queue
-import threading
 
-'''
-url解码
-'''
-def parse(imgHash, constant): 
+
+def parse(img_hash, constant):
+    '''解析哈希过后的img url'''
     q = 4
-    hashlib.md5()   
-    constant = md5(constant)
-    o = md5(constant[0:16])
-    n = md5(constant[16:32])    
-    l = imgHash[0:q]
-    c = o + md5(o + l)
-    imgHash = imgHash[q:]
-    k = decode_base64(imgHash)
-    h =list(range(256))
+    # hashlib.md5()
+    constant = parse_md5(constant)
+    o = parse_md5(constant[0:16])
+    # n = parse_md5(constant[16:32])  # unused
+    l = img_hash[0:q]
+    c = o + parse_md5(o + l)
+    img_hash = img_hash[q:]
+    k = decode_base64(img_hash)
+    h = list(range(256))
 
     b = list(range(256))
 
-    for g in range(0,256):
+    for g in range(0, 256):
         b[g] = ord(c[g % len(c)])
 
-    f=0
-    for g in range(0,256):
-        f = (f+h[g]+b[g]) % 256
+    f = 0
+    for g in range(0, 256):
+        f = (f + h[g] + b[g]) % 256
         tmp = h[g]
         h[g] = h[f]
         h[f] = tmp
-        
+
     result = ""
-    p=0
-    f=0
-    for g in range(0,len(k)):   
-        p = (p + 1) % 256;
+    p = 0
+    f = 0
+    for g in range(0, len(k)):
+        p = (p + 1) % 256
         f = (f + h[p]) % 256
         tmp = h[p]
         h[p] = h[f]
         h[f] = tmp
         result += chr(k[g] ^ (h[(h[p] + h[f]) % 256]))
     result = result[26:]
-    
+
     return result
 
-def md5(src):
-    m = hashlib.md5()   
-    m.update(src.encode("utf8"))  
+
+def parse_md5(src):
+    '''提供parse()函数需要的md5()'''
+    m = hashlib.md5()
+    m.update(src.encode("utf8"))
     return m.hexdigest()
 
+
 def decode_base64(data):
-    missing_padding=4-len(data)%4
+    '''提供parse()所需要的decode_base64()'''
+    missing_padding = 4 - len(data) % 4
     if missing_padding:
-        data += '='* missing_padding
+        data += '=' * missing_padding
     return base64.b64decode(data)
 
-headers={
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,\
+    image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cookie':'nsfw-click-load=off; bad-click-load=on; gif-click-load=on',  # 关闭NSFW
+    'Cache-Control': 'max-age=0',
+    'Host': 'jandan.net',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.99 Safari/537.36'
 }
 
-'''
-页面抓取类
-'''
-class Spider(threading.Thread):  
 
-    def __init__(self,url_List,proxies,urlMannager):  
-        threading.Thread.__init__(self)  
-        self.url_List=url_List
-        self.proxies=proxies
-        self.urlMannager=urlMannager 
+def get_soup_list(url='http://jandan.net/ooxx', page_num=3):
+    '''返回包含page_num个beautifsoup对象的列表 page_num是返回的页面数目
+    已验证可爬取无聊图http://jandan.net/pic 和 妹子图 http://jandan.net/ooxx'''
+    pages = []
+    for i in range(page_num):
+        time.sleep(1)
+        print('Url:', url)
+        html = requests.get(url, headers=HEADERS).text
+        soup = BeautifulSoup(html, 'lxml')
+        pages.append(soup)
+        url = 'http:' + soup.select('.previous-comment-page')[0]['href']  # 得到下一页的url
+    return pages
 
-    def get_Page(self,page_Url,proxies,urlMannager):
-        html=requests.get(page_Url,headers=headers,proxies=proxies,timeout=3).text
-        page=BeautifulSoup(html,"lxml")
 
-        '''
-        获取js文件地址从而得到constant常量
-        '''
-        try:
-            model=re.findall(r'.*<script\ssrc=\"\/\/(cdn.jandan.net\/static\/min.*?)\"><\/script>.*',html)
-            jsFileUrl="http://"+model[len(model)-1]     #页面上可能有两个地址，取最后一个匹配的地址
-        except Exception as e:
-            print(e)
-        jsFile=requests.get(jsFileUrl,headers=headers,proxies=proxies,timeout=3).text
+JS_FILE = None
+def get_constant_and_hash(soup):
+    '''得到解析函数所需的常量字符串和哈希后的地址 参数为beautifulsoup对象'''
+    global JS_FILE  # 全局变量应避免使用 以后改掉
+    if JS_FILE is None:  # js文件只获取一次 多次会跳转页面
+        j = soup.find('script', {'src': re.compile(
+            r'\/\/cdn.jandan.net\/static\/min.*?')})
+        js_file_url = "http://" + j['src'][2:]
+        JS_FILE = requests.get(js_file_url, headers=HEADERS).text
+    cons = re.search(r'.*f_\w+\(e,\"(\w+)\".*', JS_FILE)  # 得到原js函数中的一个用于解析的字符串实参
+    constant = cons.group(1)
 
-        constant=re.search(r'.*f_\w+\(e,\"(\w+)\".*',jsFile).group(1)
-    
-        '''
-        向parse函数传入constant常量和img-hash得到图片地址
-        '''
-        for item in page.select('.img-hash'):
-            imgUrl='http:'+parse(item.text,constant)
-            urlMannager.addNewUrl(imgUrl)
+    result_list = []
+    for item in soup.select('.img-hash'):
+        result_list.append(item.text)  # 得到所有哈希过后的图片地址
+    return constant, result_list
 
-    def run(self):
-        for url in self.url_List:
-            self.get_Page(url,self.proxies,self.urlMannager)
 
-'''
-程序入口
-'''
-def main(amount,firstIndex):
-    urlMannager=UrlMannager()
-    proxies={'http':''}             #尚未添加ip代理功能，程序已能正常运行
+def get_highquality_index(soup):
+    '''筛选图片 得到评价相对好的图片 参数为beautifulsoup对象'''
+    votes_list = soup.find('ol', {'class':'commentlist'}).find_all('div', {'class':'jandan-vote'})
+    like_socres = []  # 每张图片的oo数
+    unlike_socres = []  # 每张图片的xx数
+    index_list = []  # 高质量图片的下标
+    for vote in votes_list:
+        like = vote.find('span', {'class':'tucao-like-container'}).find('span').string
+        like_socres.append(int(like))
+        unlike = vote.find('span', {'class':'tucao-unlike-container'}).find('span').string
+        unlike_socres.append(int(unlike))
+    for index in map(like_socres.index, like_socres):
+        # 选取oo大于xx三倍 且 xx小于25的图片
+        if (like_socres[index] > unlike_socres[index]*3) and (unlike_socres[index] < 25):
+            index_list.append(index)
+    return index_list
 
-    '''
-    多线程抓取页面地址
-    '''
-    page_Url=[]            #待抓取页面
-    for i in range(amount):
-        page_Url.append('http://jandan.net/ooxx/page-'+str(firstIndex-i))
-    page_threads = []  
-    t_amount=10 if len(page_Url)>10 else len(page_Url)  #页面抓取线程数
-    for i in range(t_amount):  
-        t = Spider(page_Url[int((len(page_Url))/t_amount)*i:int((len(page_Url))/t_amount)*(i+1)],proxies,urlMannager)
-        page_threads.append(t)  
-    for t in page_threads:  
-        t.start()
-    for t in page_threads:  
-        t.join()
 
-    img_threads = []  
-    for i in range(10):             #固定10个线程用于下载图片
-        t = Download(urlMannager)
-        img_threads.append(t)  
-    for t in img_threads:  
-        t.start()
-    for t in img_threads:  
-        t.join()
+random.seed(datetime.datetime.now())
+def get_random_index(pic_num, pic_num_max):
+    '''在一定index范围内获得随机的下标 或 选取全部下标 参数为得到的图片数与页面最大图片数'''
+    index_list = []
+    if pic_num < pic_num_max*0.75:  # 防止传入参数超过边界 选取数接近总数时随机效率会很低 故取0.75*max
+        for i in range(pic_num):
+            index = int(random.random() * pic_num_max)
+            while index in index_list:  # 随机选取的图片已存在时 再次选取
+                index = int(random.random() * pic_num_max)
+            index_list.append(index)
+    else:  # 参数越界后选取全部图片
+        for i in range(pic_num_max):
+            index_list.append(i)
+    return index_list
 
-L = threading.Lock()
 
-'''
-图片下载类
-'''
-class Download(threading.Thread):  
-    def __init__(self, urlMannager):  
-        threading.Thread.__init__(self)  
-        self.urlMannager=urlMannager 
+def spider(soup, pic_num=3, Mode='Random'):
+    '''处理解析后的地址 选择图片 保存图片
+    参数为beautifulsoup对象 每页爬取图片数pic_num 选取图片的方式Mode有'Random'与'HighQuality'两种方式'''
+    result = get_constant_and_hash(soup)
+    constant = result[0]  # 用于parse()的常量字符串
+    hash_list = result[1]  # 哈希过后的图片地址list
+    index_list = []  # 需要保存的图片在hash_list中的下标
 
-    def download_Img(self,url):
-        isGif=re.match(r'(.*\.sinaimg\.cn\/)(\w+)(\/.+\.gif)',url)
-        if isGif:
-            url=isGif.group(1)+'large'+isGif.group(3)
+    if Mode is 'Random':  # 根据参数选择图片下载模式
+        index_list = get_random_index(pic_num, len(hash_list))
+    elif Mode is 'HighQuality':
+        index_list = get_highquality_index(soup)
+    else:
+        return None
 
-        extensionName=re.match(r'.*(\.\w+)',url).group(1)       #图片扩展名
+    for index in index_list:
+        img_hash = hash_list[index]
+        url = 'http:' + parse(img_hash, constant)
+        replace = re.match(r'(.*\.sinaimg\.cn\/)(\w+)(\/.+\.gif)', url)
+        if replace:
+            url = replace.group(1) + 'large' + replace.group(3)  # 获得原图
+        ext_match = re.match(r'.*(\.\w+)', url)
+        extension_name = ext_match.group(1)  # 获得图片扩展名
+        md5 = hashlib.md5()
+        md5.update(url.encode("utf8"))  # 用md5后的图片url作为文件名
+        file_name = md5.hexdigest() + extension_name
+        print('Pic: ', file_name)
+        with open(file_name, 'wb') as pic:
+            HEADERS['host'] = 'wx3.sinaimg.cn'  # 此处利用了HEADERS 但会使其不能再用于获取html
+            pic.write(requests.get(url, headers=HEADERS).content)
+            pic.close()
 
-        L.acquire()
-        if not os.path.exists('img'):
-            os.mkdir('img')
-        with open ('img/'+str(len(os.listdir('./img')))+extensionName, 'wb') as f:
-            headers['Host']='wx3.sinaimg.cn'
-            f.write(requests.get(url,headers=headers).content)
-            f.close()
-        L.release()
-        
-    def run(self):  
-        while not self.urlMannager.isEmpty():
-            imgUrl=self.urlMannager.getNewUrl()
-            self.download_Img(imgUrl)
-            self.urlMannager.addOldUrl(imgUrl)
 
-'''
-url仓库，提供url更新以及记录功能
-'''
-class UrlMannager:
-    def __init__(self):
-        self.url_used=[]
-        self.url_target=queue.Queue()
-        if os.path.exists('url.txt'):
-            with open('url.txt','r') as f:
-                for eachline in f.readlines():
-                    self.url_used.append(eachline.strip())
-        else:
-            open("url.txt",'w')
+def main():
+    '''main'''
+    # soup_list = get_soup_list(page_num=5)  # 妹子图
+    soup_list = get_soup_list('http://jandan.net/pic', 5)  # 无聊图
+    for soup in soup_list:
+        time.sleep(2)
+        spider(soup, pic_num=100, Mode='HighQuality')
 
-    def getNewUrl(self):
-        return self.url_target.get()
 
-    def isEmpty(self):
-        return self.url_target.empty()
-
-    def addNewUrl(self,newUrl):
-        if newUrl in self.url_used:
-            pass
-        else:
-            self.url_target.put(newUrl)
-
-    def addOldUrl(self,oldUrl):
-        self.url_used.append(oldUrl)
-        with open('url.txt','a') as f:
-            f.write(oldUrl+'\n')
-
-if __name__=='__main__':
-
-    firstPage=requests.get('http://jandan.net/ooxx',headers=headers).text
-    firstIndex=int(re.search(r'.*current.*\">\[(.*?)\].*',firstPage)[1])  #首页页码
-    
-    amount=input('请输入抓取页数后按回车开始(小于'+str(firstIndex)+'，从首页开始计数):')
-    main(int(amount),firstIndex)                   #抓取首页开始的前amount页的图片
+if __name__ == '__main__':
+    main()
